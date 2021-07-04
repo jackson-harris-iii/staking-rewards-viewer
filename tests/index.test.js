381d77ecf41ece3cd0d63210fd7a5d5cde22e5a8
Jackson <Jackson.harris3@gmail.com>
2021-07-04 16:46:38 -0700

    write out needed tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,11 +4,21 @@
 import { render, screen } from "@testing-library/react";
 import HomePage from "../pages/index.js";
 
-describe("MyApp", () => {
+describe("Staking Rewards Viewer Homepage", () => {
   it("renders without crashing", () => {
     render(<HomePage />);
     expect(
       screen.getByRole("heading", { name: "Staking Rewards Viewer" })
     ).toBeInTheDocument();
   });
+
+  //write test for search button
+
+  //write test for new wallet adress entry
+
+  //write test to mock search request/render summary
+
+  //write test to render daily dot data
+
+  // write test for chart interaction
 });
